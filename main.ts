06f409438a41e637fe849147b55d818ca9210fcb
Alice Brookes <72383278+aliblol@users.noreply.github.com>
2025-05-20 20:47:31 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -16,6 +16,10 @@ radio.onReceivedString(function (receivedString) {
         cuteBot.backforward()
     } else if (receivedString == "stop") {
         cuteBot.stopcar()
+    } else if (receivedString == "RainbowLights") {
+        trick = true
+        rainbowLights()
+        trick = false
     }
 })
 // When button B is pressed, go to the next group
@@ -24,6 +28,16 @@ input.onButtonPressed(Button.B, function () {
     basic.showNumber(currentGroup)
     radio.setGroup(currentGroup)
 })
+function rainbowLights () {
+    strip.showRainbow(1, 360)
+    for (let index = 0; index < 20; index++) {
+        strip.rotate(1)
+        strip.show()
+        basic.pause(100)
+    }
+    strip.clear()
+    strip.show()
+}
 // Optional: If a number value is received (like tilt), store it
 radio.onReceivedValue(function (name, value) {
     if (name == "Pitch") {
@@ -31,6 +45,9 @@ radio.onReceivedValue(function (name, value) {
     }
 })
 let pitchValue = 0
+let trick = false
+let strip: neopixel.Strip = null
 let currentGroup = 0
 // Set the group (channel) for radio communication
 radio.setGroup(currentGroup)
+strip = neopixel.create(DigitalPin.P15, 8, NeoPixelMode.RGB)
