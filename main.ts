8870d95feffcf673e08ea04bd34e62cc6f62852b
Alice Brookes <72383278+aliblol@users.noreply.github.com>
2026-03-10 21:23:53 +0000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,36 +1,42 @@
+function setGroup () {
+    basic.showNumber(currentGroup)
+    radio.setGroup(currentGroup)
+}
 // When button A is pressed, go to the previous group
 input.onButtonPressed(Button.A, function () {
     currentGroup = currentGroup - 1
-    basic.showNumber(currentGroup)
-    radio.setGroup(currentGroup)
+    setGroup()
 })
 // When a string message is received, make the robot move
 radio.onReceivedString(function (receivedString) {
-    if (receivedString == "forward") {
+    if (receivedString == forward) {
         cuteBot.forward()
-    } else if (receivedString == "left") {
+    } else if (receivedString == left) {
         cuteBot.turnleft()
-    } else if (receivedString == "right") {
+    } else if (receivedString == right) {
         cuteBot.turnright()
-    } else if (receivedString == "backward") {
+    } else if (receivedString == backward) {
         cuteBot.backforward()
-    } else if (receivedString == "stop") {
+    } else if (receivedString == stop) {
         cuteBot.stopcar()
     }
 })
 // When button B is pressed, go to the next group
 input.onButtonPressed(Button.B, function () {
     currentGroup = currentGroup + 1
-    basic.showNumber(currentGroup)
-    radio.setGroup(currentGroup)
+    setGroup()
 })
-// Optional: If a number value is received (like tilt), store it
-radio.onReceivedValue(function (name, value) {
-    if (name == "Pitch") {
-        pitchValue = value
-    }
-})
-let pitchValue = 0
+let right = ""
+let left = ""
+let forward = ""
+let backward = ""
+let stop = ""
 let currentGroup = 0
 // Set the group (channel) for radio communication
 radio.setGroup(currentGroup)
+stop = "stop"
+backward = "backward"
+forward = "forward"
+left = "left"
+right = "right"
+let RainbowLights = "RainbowLights"
